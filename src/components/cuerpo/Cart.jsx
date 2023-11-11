46ade26b9d1b6b21c53d702348ff652ad5033e43
brunovalenzisi <130124652+brunovalenzisi@users.noreply.github.com>
2023-11-11 12:01:15 -0300

    proyectoFinal-Valenzisi

--- src/components/cuerpo/Cart.jsx
+++ src/components/cuerpo/Cart.jsx
@@ -19,7 +19,7 @@ const Cart = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4">
-      <h1 className="text-xs sm:text-xs md:text-s lg:text-2xl xl:text-2xl font-bold mb-2">Resumen del Carrito</h1>
+      <h1 className="text-xs sm:text-xs md:text-s lg:text-xl xl:text-xl font-bold mb-2">Resumen del Carrito</h1>
       {cart.length === 0 ? ( 
         <div>
           <p>El carrito está vacío.</p>
--- src/components/cuerpo/CartItem.jsx
+++ src/components/cuerpo/CartItem.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const CartItem = ({ item, addToCart, restToCart, removeItem }) => {
   return (
 
-    <tr className="cart-card text-xs sm:text-xs md:text-s lg:text-xl xl:text-xl px-1">
+    <tr className="cart-card text-xs sm:text-xs md:text-s lg:text-l xl:text-l px-1">
         <td>{item.producto}</td>
-      <td className="text-center text-xs sm:text-xs md:text-s lg:text-xl xl:text-xl px-1">
+      <td className="text-center text-xs sm:text-xs md:text-s lg:text-l xl:text-l px-1">
         
         {item.cantidad}
         
       </td>
-      <td className="text-xs sm:text-xs md:text-s lg:text-xl xl:text-xl px-1" >${parseInt(item.precio.replace(/\./g, "")).toFixed(2)}</td>
+      <td className="text-xs sm:text-xs md:text-s lg:text-l xl:text-l px-1" >${parseInt(item.precio.replace(/\./g, "")).toFixed(2)}</td>
       <td >${(parseInt(item.precio.replace(/\./g, "")) * item.cantidad).toFixed(2)}</td>
       <td>
       <button className="btn-add"  onClick={() => restToCart(item)}><h1 >-</h1> </button>
